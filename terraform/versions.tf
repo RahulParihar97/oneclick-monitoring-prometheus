56a6b3ddd6725880939451ee3e8f393628714537
Rahul Parihar <121428206+RahulParihar97@users.noreply.github.com>
2026-07-04 20:31:54 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,12 @@
 terraform {
+  # Enforce Terraform version
+  required_version = ">= 1.5.0"
 
+  # Define required providers
   required_providers {
-
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.50"
     }
-
   }
-
 }
