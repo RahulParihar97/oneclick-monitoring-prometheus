af36eb220049a277ea6e70dc4599e1cf944cc2b2
Rahul Parihar <121428206+RahulParihar97@users.noreply.github.com>
2026-07-04 20:39:45 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,13 @@ output "app_server_1_private_ip" {
 output "app_server_2_private_ip" {
   value = module.ec2.private_ips["app-server-2"]
 }
+
+output "ssh_config" {
+  value = <<EOT
+Host bastion-server
+  HostName ${aws_instance.bastion.public_ip}
+  User ubuntu
+  IdentityFile ${path.module}/generated/id_rsa
+EOT
+}
+
